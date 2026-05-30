4ad85bdd88a89638c82130399d4d881d7921a1ae
cesartalves <cesartalvez@gmail.com>
2026-05-30 17:32:39 -0300

    basic number form

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem 'aws-sdk-dynamodb'
+gem 'dotenv'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,42 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def subscribe
+    ddi = params[:ddi].to_s.strip
+    ddd = params[:ddd].to_s.strip
+    number = params[:number].to_s.strip
+
+    unless ddi.match?(/\A\d{1,3}\z/)
+      redirect_to root_path, alert: "DDI inválido"
+      return
+    end
+
+    unless ddd.match?(/\A\d{2}\z/)
+      redirect_to root_path, alert: "DDD inválido"
+      return
+    end
+
+    unless number.match?(/\A9\d{8}\z/)
+      redirect_to root_path, alert: "Número de celular inválido"
+      return
+    end
+
+    full_number = "#{ddi}#{ddd}#{number}"
+
+    dynamodb = ::Aws::DynamoDB::Client.new(
+      region: ENV['AWS_REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+
+    response = dynamodb.put_item(
+      table_name: 'numeros-projeto-espirita',
+      item: {
+        'number' => full_number
+      }
+    )
+
+    redirect_to root_path, notice: "Número recebido: #{full_number}"
+  end
 end
